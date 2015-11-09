3a8d3f5c0eda51496e49526081a4e8e2b5996fbe
xingjiahe <564710948@qq.com>
2015-11-09 13:19:51 +0800

    xingjiahetijiao

---
+++ kk/kkkkk/kkkkk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kkkkk
+//
+//  Created by 邢家赫 on 15/11/9.
+//  Copyright © 2015年 邢家赫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kk/kkkkk/kkkkk/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  kkkkk
+//
+//  Created by 邢家赫 on 15/11/9.
+//  Copyright © 2015年 邢家赫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
